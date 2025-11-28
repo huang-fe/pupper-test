8a75720752ddb8b2a8c21f68065af8a81221c889
Felicity H <felicityh15@gmail.com>
2025-11-27 18:52:59 -0800

    added animation controller

---
+++ test_lab4/newsit.py
@@ -0,0 +1,282 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Float64MultiArray
+import numpy as np
+
+# np.set_printoptions(precision=3, suppress=True)
+# -----------------------------
+# Transform helpers
+# -----------------------------
+def rotation_x(angle):
+    return np.array(
+        [
+            [1, 0, 0, 0],
+            [0, np.cos(angle), -np.sin(angle), 0],
+            [0, np.sin(angle), np.cos(angle), 0],
+            [0, 0, 0, 1],
+        ]
+    )
+def rotation_y(angle):
+    return np.array(
+        [
+            [np.cos(angle), 0, np.sin(angle), 0],
+            [0, 1, 0, 0],
+            [-np.sin(angle), 0, np.cos(angle), 0],
+            [0, 0, 0, 1],
+        ]
+    )
+def rotation_z(angle):
+    return np.array(
+        [
+            [np.cos(angle), -np.sin(angle), 0, 0],
+            [np.sin(angle), np.cos(angle), 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ]
+    )
+def translation(x, y, z):
+    return np.array(
+        [
+            [1, 0, 0, x],
+            [0, 1, 0, y],
+            [0, 0, 1, z],
+            [0, 0, 0, 1],
+        ]
+    )
+
+# =============================
+#      INVERSE KINEMATICS
+# =============================
+
+class InverseKinematics(Node):
+    def __init__(self):
+        super().__init__('sit_stand_kinematics')
+
+        # Sub & Pub
+        self.joint_subscription = self.create_subscription(
+            JointState,
+            'joint_states',
+            self.joint_state_cb,
+            10
+        )
+        self.command_pub = self.create_publisher(
+            Float64MultiArray,
+            '/forward_command_controller/commands',
+            10
+        )
+
+        self.joint_positions = None
+        self.joint_velocities = None
+        self.target_joint_positions = None
+        self.counter = 0
+
+        # modes: "stand", "sit", "idle"
+        self.mode = "stand"
+
+        # # These will be filled by trajectory builder
+        # self.motion_cache = None
+        # self.motion_index = 0
+
+        # Sitting & standing default foot locations
+        self.default_stand_ee = {
+            0: np.array([0.06, -0.09, -0.14]),   # FR
+            1: np.array([0.06,  0.09, -0.14]),   # FL
+            2: np.array([-0.11,-0.09, -0.14]),   # BR
+            3: np.array([-0.11, 0.09, -0.14])    # BL
+        }
+
+        self.default_sit_ee = {
+            0: np.array([0.06, -0.09, -0.14]),   # front legs unchanged
+            1: np.array([0.06,  0.09, -0.14]),
+            2: np.array([-0.03, -0.09, -0.05]),  # back legs folded under
+            3: np.array([-0.03,  0.09, -0.05])
+        }
+
+        # FK for each leg index
+        self.fk_functions = [
+            self.fr_leg_fk, self.fl_leg_fk,
+            self.br_leg_fk, self.bl_leg_fk
+        ]
+
+        # Timers
+        self.ik_timer = self.create_timer(1/100.0, self.ik_cb)
+        self.pd_timer = self.create_timer(1/200.0, self.pd_cb)
+
+    # -----------------------------
+    # Forward Kinematics for each leg
+    # -----------------------------
+    def fr_leg_fk(self, theta):
+        T_RF_0_1 = translation(0.07500, -0.08350, 0) @ rotation_x(1.57080) @ rotation_z(theta[0])
+        T_RF_1_2 = rotation_y(-1.57080) @ rotation_z(theta[1])
+        T_RF_2_3 = translation(0, -0.04940, 0.06850) @ rotation_y(1.57080) @ rotation_z(theta[2])
+        T_RF_3_ee = translation(0.06231, -0.06216, 0.01800)
+        T_RF_0_ee = T_RF_0_1 @ T_RF_1_2 @ T_RF_2_3 @ T_RF_3_ee
+        return T_RF_0_ee[:3, 3]
+
+    def fl_leg_fk(self, theta):
+        # T_0_1 (base_link to leg_front_l_1)
+        theta1, theta2, theta3 = theta
+        T_0_1 = translation(0.07500, -0.0445, 0) @ rotation_x(1.57080) @ rotation_z(theta1)
+        T_1_2 = translation(0, 0, 0.039) @ rotation_y(-1.57080) @ rotation_z(theta2)
+        T_2_3 = translation(0, -0.0494, 0.0685) @ rotation_y(1.57080) @ rotation_z(theta3)
+        T_3_ee = translation(0.06231, -0.06216, 0.018) 
+        T_0_ee = T_0_1 @ T_1_2 @ T_2_3 @ T_3_ee
+        return T_0_ee[:3,3]
+
+    def br_leg_fk(self, theta):
+        T_RF_0_1 = translation(-0.07500, -0.08350, 0) @ rotation_x(1.57080) @ rotation_z(theta[0])
+        T_RF_1_2 = rotation_y(-1.57080) @ rotation_z(theta[1])
+        T_RF_2_3 = translation(0, -0.04940, 0.06850) @ rotation_y(1.57080) @ rotation_z(theta[2])
+        T_RF_3_ee = translation(0.06231, -0.06216, 0.01800)
+        T_RF_0_ee = T_RF_0_1 @ T_RF_1_2 @ T_RF_2_3 @ T_RF_3_ee
+        return T_RF_0_ee[:3, 3]
+
+    def bl_leg_fk(self, theta):
+        theta1, theta2, theta3 = theta
+        T_0_1 = translation(-0.07500, 0.0445, 0) @ rotation_x(-1.57080) @ rotation_z(theta1)
+        T_1_2 = translation(0, 0, 0.039) @ rotation_y(-1.57080) @ rotation_z(theta2)
+        T_2_3 = translation(0, -0.0494, 0.0685) @ rotation_y(1.57080) @ rotation_z(theta3)
+        T_3_ee = translation(0.06231, -0.06216, 0.018) 
+        T_0_ee = T_0_1 @ T_1_2 @ T_2_3 @ T_3_ee
+        return T_0_ee[:3,3]
+
+    def forward_kinematics(self, theta):
+        return np.concatenate([self.fk_functions[i](theta[3*i: 3*i+3]) for i in range(4)])
+
+    # -----------------------------
+    # Inverse Kinematics (gradient descent)
+    # -----------------------------
+
+    def inverse_kinematics_single_leg(self, target_ee, leg_index, initial_guess=[0, 0, 0]):
+        leg_forward_kinematics = self.fk_functions[leg_index]
+
+        def cost_function(theta):
+            current_position = leg_forward_kinematics(theta)
+            cost = np.linalg.norm(target_ee-current_position)
+            return cost **2
+
+        def gradient(theta, epsilon=1e-3):
+            grad = np.zeros(3)
+            for i in range(len(theta)):
+                theta_plus = theta.copy()
+                theta_minus = theta.copy()
+                theta_plus[i] += epsilon
+                theta_minus[i] -= epsilon
+
+                cost_plus = cost_function(theta_plus)
+                cost_minus = cost_function(theta_minus)
+
+                grad[i] = (cost_plus - cost_minus) / (2 * epsilon)
+            return grad
+
+        theta = np.array(initial_guess)
+        learning_rate = 10 # TODO:[already done] paste lab 3 inverse kinematics here
+        max_iterations = 20 # TODO: [already done] paste lab 3 inverse kinematics here
+        tolerance = 0.0002 # TODO: [already done] paste lab 3 inverse kinematics here
+
+        for _ in range(max_iterations):
+            grad = gradient(theta)
+            if np.sum(np.abs(target_ee-theta)) < tolerance:
+                break
+            theta = theta - learning_rate * grad
+
+        return theta
+
+    # -----------------------------
+    # Trajectory generator
+    # -----------------------------
+    def generate_motion(self, target_ee_dict, duration=1.5, dt=0.02):
+        steps = int(duration/dt)
+
+        # Current EE positions
+        current = self.forward_kinematics(self.joint_positions).reshape(4,3)
+
+        trajectory = []
+
+        for t in range(steps):
+            s = t / (steps-1)
+            interp_targets = []
+
+            # interpolate each foot
+            for leg in range(4):
+                start = current[leg]
+                end = target_ee_dict[leg]
+                interp = (1-s)*start + s*end
+                interp_targets.append(interp)
+
+            # IK for all legs
+            joint_list = []
+            for leg in range(4):
+                ee = interp_targets[leg]
+                j = self.inverse_kinematics_single_leg(ee, leg)
+                joint_list.append(j)
+
+            trajectory.append(np.concatenate(joint_list))
+
+        return np.array(trajectory)
+
+    # -----------------------------
+    # ROS Callbacks
+    # -----------------------------
+    def joint_state_cb(self, msg):
+        names = [
+            'leg_front_r_1','leg_front_r_2','leg_front_r_3',
+            'leg_front_l_1','leg_front_l_2','leg_front_l_3',
+            'leg_back_r_1','leg_back_r_2','leg_back_r_3',
+            'leg_back_l_1','leg_back_l_2','leg_back_l_3'
+        ]
+        self.joint_positions = np.array([msg.position[msg.name.index(n)] for n in names])
+        self.joint_velocities = np.array([msg.velocity[msg.name.index(n)] for n in names])
+
+    def ik_cb(self):
+        if self.joint_positions is None:
+            return
+
+        # Build trajectory if needed
+        if self.motion_cache is None:
+            if self.mode == "sit":
+                self.motion_cache = self.generate_motion(self.default_sit_ee)
+                self.motion_index = 0
+            elif self.mode == "stand":
+                self.motion_cache = self.generate_motion(self.default_stand_ee)
+                self.motion_index = 0
+            else:
+                return
+
+        # Follow trajectory
+        if self.motion_index < len(self.motion_cache):
+            self.target_joint_positions = self.motion_cache[self.motion_index]
+            self.motion_index += 1
+        else:
+            # Hold last frame
+            self.target_joint_positions = self.motion_cache[-1]
+
+    def pd_cb(self):
+        if hasattr(self, "target_joint_positions"):
+            msg = Float64MultiArray()
+            msg.data = self.target_joint_positions.tolist()
+            self.command_pub.publish(msg)
+
+def main():
+    rclpy.init()
+    node = InverseKinematics()
+
+    try:
+        print("\nCommands:")
+        print("  node.mode='sit'   → robot sits")
+        print("  node.mode='stand' → robot stands up\n")
+
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        print("Shutting down.")
+    finally:
+        zero = Float64MultiArray()
+        zero.data = [0.0]*12
+        node.command_pub.publish(zero)
+        node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
